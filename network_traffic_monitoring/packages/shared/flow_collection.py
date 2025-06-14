870d9d8fa189683d4e515fe7436842109503dfec
VinhTin-AQUA <tinhovinh@gmail.com>
2025-06-14 16:06:56 +0700

    update

--- network_traffic_monitoring/packages/shared/flow_collection.py
+++ network_traffic_monitoring/packages/shared/flow_collection.py
@@ -1,7 +1,7 @@
 import requests
 from pandas.core.frame import DataFrame
 import json
-from ..prediction.tabnet_prediction import TabNetClassifier
+from ..prediction.tabnet_prediction import TabnetPrediction
 from packages.shared.shared_data import SharedApi
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -14,7 +14,7 @@ class FlowCollection:
             cls._instance = super(FlowCollection, cls).__new__(cls)
             cls._instance._queue = []     # Mảng chứa các đối tượng
             cls._instance._size = 0       # Số lượng đối tượng
-            cls._model = TabNetClassifier()
+            cls._model = TabnetPrediction()
             cls._share_api = SharedApi()
         return cls._instance
 
@@ -57,15 +57,15 @@ class FlowCollection:
                     "data": message_dict
                 }
 
-                response = session.post(self._share_api.api_base + '/tracking/flow-tracking', json=payload, headers=headers, verify=False)
+                # response = session.post(self._share_api.api_base + '/tracking/flow-tracking', json=payload, headers=headers, verify=False)
 
-                if response.status_code == 200 or response.status_code == 201:  # Kiểm tra nếu request thành công
-                    data = response.json()  # Chuyển đổi dữ liệu JSON thành dict
-                    print(data)
-                    # print("gui thanh cong")
-                    pass
-                else:
-                    print(f"Lỗi {response.status_code}")
+                # if response.status_code == 200 or response.status_code == 201:  # Kiểm tra nếu request thành công
+                #     data = response.json()  # Chuyển đổi dữ liệu JSON thành dict
+                #     print(data)
+                #     # print("gui thanh cong")
+                #     pass
+                # else:
+                #     print(f"Lỗi {response.status_code}")
 
     # def __str__(self):
     #     return f"Queue({self._queue}) - Size: {self._size}"
